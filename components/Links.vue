a4cc431097d9333e9cb552f9e1f5191a71ebcb1e
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-05-23 09:59:27 -0300

    Adiciona componente Link

---
+++ components/Links.vue
@@ -0,0 +1,32 @@
+<template>
+  <g>
+    <path
+      id="link"
+      :d="`M ${source.x}, ${source.y} L ${target.x}, ${target.y}`"
+    />
+  </g>
+</template>
+
+<script>
+export default {
+  name: 'Link',
+  props: {
+    target: {
+      type: Object
+    },
+    source: {
+      type: Object
+    },
+    id: {
+      type: String
+    }
+  }
+}
+</script>
+<style>
+#link {
+  fill: none;
+  stroke: #000000;
+  stroke-width: 1px;
+}
+</style>
