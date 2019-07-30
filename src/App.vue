bb5bbfaab578b29599b2393b1d813f32f4470497
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2019-07-29 23:34:48 -0300

    Modifica repositório para vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <graphic-visualization />
+</template>
+
+<script>
+import GraphicVisualization from './components/GraphicVisualization.vue';
+
+export default {
+  name: 'app',
+  components: {
+    GraphicVisualization,
+  },
+};
+</script>
