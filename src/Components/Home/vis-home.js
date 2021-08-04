ae5b086ae355ff60f78d8536c283b2664d494f5f
Beatriz Miranda <beatriza.dmiranda@gmail.com>
2021-08-04 20:48:54 -0300

    Adding visualization in the home component

---
+++ src/Components/Home/vis-home.js
@@ -0,0 +1,142 @@
+import VegaLite from 'react-vega-lite';
+import ReactDOM from 'react-dom';
+
+export const spec = {
+    "$schema": "https://vega.github.io/schema/vega/v5.json",
+    "description": "A node-link diagram with force-directed layout, depicting character co-occurrence in the novel Les Misérables.",
+    "width": 600,
+    "height": 700,
+    "padding": 0,
+    "autosize": "fit",
+  
+    "signals": [
+      { "name": "cx", "update": "width / 2" },
+      { "name": "cy", "update": "height / 2" },
+      {
+        "description": "State variable for active node fix status.",
+        "name": "fix", "value": false,
+        "on": [
+          {
+            "events": "symbol:mouseout[!event.buttons], window:mouseup",
+            "update": "false"
+          },
+          {
+            "events": "symbol:mouseover",
+            "update": "fix || true"
+          },
+          {
+            "events": "[symbol:mousedown, window:mouseup] > window:mousemove!",
+            "update": "xy()",
+            "force": true
+          }
+        ]
+      },
+      {
+        "description": "Graph node most recently interacted with.",
+        "name": "node", "value": null,
+        "on": [
+          {
+            "events": "symbol:mouseover",
+            "update": "fix === true ? item() : node"
+          }
+        ]
+      }
+    ],
+  
+    "data": [
+      {
+        "name": "node-data",
+        "url": "https://gist.githubusercontent.com/beatrizadm/35c80fb4fdd3c8d108baaf254117ee32/raw/e383284cad955445cb53ad7a00cd842675abb10b/visu-data.json",
+        "format": {"type": "json", "property": "nodes"}
+      },
+      {
+        "name": "link-data",
+        "url": "https://gist.githubusercontent.com/beatrizadm/35c80fb4fdd3c8d108baaf254117ee32/raw/e383284cad955445cb53ad7a00cd842675abb10b/visu-data.json",
+        "format": {"type": "json", "property": "links"}
+      }
+    ],
+  
+    "scales": [
+      {
+        "name": "color",
+        "type": "ordinal",
+        "domain": {"data": "node-data", "field": "group"},
+        "range": ["#FFE4C4", "#347953", "#a6e8c8", "#ff8b52", "#94ff75", "#f9eb39", "#95C623"]
+      }
+    ],
+  
+    "marks": [
+      {
+        "name": "nodes",
+        "type": "symbol",
+        "zindex": 1,
+  
+        "from": {"data": "node-data"},
+        "on": [
+          {
+            "trigger": "fix",
+            "modify": "node",
+            "values": "fix === true ? {fx: node.x, fy: node.y} : {fx: fix[0], fy: fix[1]}"
+          },
+          {
+            "trigger": "!fix",
+            "modify": "node", "values": "{fx: null, fy: null}"
+          }
+        ],
+  
+        "encode": {
+          "enter": {
+            "fill": {"scale": "color", "field": "group"},
+            "stroke": {"value": "white"}
+          },
+          "update": {
+            "size": {"signal": "2 * 15 * 15"},
+            "cursor": {"value": "pointer"}
+          }
+        },
+  
+        "transform": [
+          {
+            "type": "force",
+            "iterations": 30000000000000000000000,
+            "static": false,
+            "signal": "force",
+            "forces": [
+              {"force": "center", "x": {"signal": "cx"}, "y": {"signal": "cy"}},
+              {"force": "collide", "radius":  15},
+              {"force": "nbody", "strength": -50},
+              {"force": "link", "links": "link-data", "distance": 50}
+            ]
+          }
+        ]
+      },
+      {
+        "type": "path",
+        "from": {"data": "link-data"},
+        "iterations": 30000000000000000000000,
+        "interactive": false,
+        "encode": {
+          "update": {
+            "stroke": {"value": "#ccc"},
+            "strokeWidth": {"value": 0.5}
+          }
+        },
+        "transform": [
+          {
+            "type": "linkpath",
+            "require": {"signal": "force"},
+            "shape": "line",
+            "sourceX": "datum.source.x", "sourceY": "datum.source.y",
+            "targetX": "datum.target.x", "targetY": "datum.target.y"
+          }
+        ]
+      }
+    ]
+  };
+
+ReactDOM.render(
+   <VegaLite spec={spec} />,
+   document.getElementById('root')
+);
+
+export default spec;
