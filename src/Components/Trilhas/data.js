cb2eebd0bbd84b3088779e709543acea9039c71f
Lucas-yago <41453379+Lucas-yago@users.noreply.github.com>
2021-10-07 21:14:27 -0300

    adiciona texto trilha de ciência de dados

--- src/Components/Trilhas/data.js
+++ src/Components/Trilhas/data.js
@@ -27,6 +27,7 @@ export default [
         name:'Analista de dados',
         description: 'Pra quem gosta de tabelas',
         color: '#95C623',
-        url: '/dados'
+        url: '/dados',
+        texto: '    O Cientista de Dados analisa problemas e encontra soluções. Para isso emprega conhecimento em Estatística, Matemática, Machine Learning, Análise de Dados e conhecimento da área de negócio. Programação aqui é usada como linguagem de script para criar uma sequência de ações para resolver um problema.    O profissional pode atuar em várias áreas como Machine Learn, Analises de Dados, Inteligência Artificial, Big Date, Redes Neurais, entre outros.',
     }
 ]
