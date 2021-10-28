d092d5b46a3e1e83efa2ac2a1b348a7dc5e134d3
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-28 11:52:44 -0300

    Textos para aplicação + estilização

--- src/Components/Trilhas/Trilhas-home.jsx
+++ src/Components/Trilhas/Trilhas-home.jsx
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';
 import data from './data'
 import Card from './Card'
-import {Link} from 'react-router-dom'
+import dataTexts from '../Pt-Br'
+import { Link } from 'react-router-dom'
 
 class Trilhas extends React.Component {
-    render() { 
+    constructor(props){
+        super(props)
+        this.state = {
+            title: this.props.title, 
+            subtitle: this.props.subtitle, 
+        }
+    }
+
+    render() {
         return (
-            <>
-                <section id="trails">
-                    <h2 className="trails__title">Trilhas</h2>
-                    <p className="trails__subtitle">Confira as trilhas de estudo que o curso pode oferecer.</p>
-                    <div className="trails__card__wrapper">
-                        {data.map(trilha => (
-                            <Link to={trilha.url} style={{ color: 'black' }}>
+            <section id="trails">
+                <h2 className="trails__title">{this.props.title}</h2>
+                <p className="trails__subtitle">{this.props.subtitle}</p>
+                <div className="trails__card__wrapper">
+                    {data.map(trilha => (
+                        <Link to={trilha.url} style={{ color: 'black' }}>
                             <Card key={trilha.name}
                                 name={trilha.name}
                                 description={trilha.description}
                                 color={trilha.color}
                             />
-                            </Link>
-                        ))}
-                    </div>
-                </section>
-            </>
+                        </Link>
+                    ))}
+                </div>
+            </section>
         )
     }
 }
- 
+
 export default Trilhas;
