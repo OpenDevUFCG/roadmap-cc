1cbd4fc3bbf9d6d44f3de3c7df88dffb77342fff
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2022-09-14 22:18:21 -0300

    ref: remove unused code and old components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 };
   
 const withImages = require('next-images');
---
+++ src/components/Dummy/Dummy.js
@@ -0,0 +1,16 @@
+import styles from './styles.module.css';
+
+export default function Dummy({
+    prop1
+}
+
+){
+    return (
+        <>
+            <div className={styles.dummyWrapper}>
+                <div className={styles.dummy}>   
+                </div>
+            </div>
+        </>
+    )
+}
