e399e4e89830e274477a88a1946e695038d3be4d
leandra.silva <leandra.silva@ccc.ufcg.edu.br>
2022-09-16 13:29:08 -0300

    adiciona dependencia styled-components e estrutura de componente exemplo

---
+++ src/components/Example/styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #49ae80;
+`
+
+export const Text = styled.p`
+    font-size: 18px;
+    color: #fff;
+`
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import styles from './Home.module.css';
 import { useTranslation } from 'react-i18next';
+import { Example } from '../../components/Example';
+import { Wrapper } from './styles';
 
 export default function Index() {
   const { t } = useTranslation();
 
   return (
-    <main>
-    </main>
+    <Wrapper>
+      <Example />
+    </Wrapper>
   );
 }
