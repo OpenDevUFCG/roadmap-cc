25b33aa435a996a65f18ace8059d85c0512dd921
Tarso Jabbes <tarsojabbes@gmail.com>
2022-09-29 18:30:38 -0300

    Implementa o componente

---
+++ src/components/LinkAncora/styles.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import Image from 'next/image';
+
+export const LinkWrapper = styled.a`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.875rem;
+    text-decoration: none;
+    font-weight: 700;
+    font-size: 1rem;
+    margin: 0;
+    color: #FEA676;
+`
