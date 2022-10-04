69c0cb045e1f1e218c8b42c0b281ddbd0f364942
Tarso Jabbes <tarsojabbes@gmail.com>
2022-10-03 21:10:06 -0300

    Inclui alterações da primeira review

--- src/components/LinkAncora/styles.jsx
+++ src/components/LinkAncora/styles.jsx
@@ -1,5 +1,14 @@
-import styled from "styled-components"
-import Image from 'next/image';
+import styled, { keyframes } from "styled-components"
+
+const increaseSize = keyframes`
+    from {
+        transform: scale(1);
+    }
+
+    to {
+        transform: scale(1.05);
+    }
+`
 
 export const LinkWrapper = styled.a`
     display: flex;
@@ -11,5 +20,10 @@ export const LinkWrapper = styled.a`
     font-weight: 700;
     font-size: 1rem;
     margin: 0;
-    color: #FEA676;
+    color: var(--orange);
+
+    &:hover {
+        animation: ${increaseSize} 0.5s ease-in-out forwards;
+    }
+    
 `
