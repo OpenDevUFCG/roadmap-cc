866ad3bc17ae590f4b461c185b6ddec7267f8e83
vitor barbosa <jose.vitor.maciel@ccc.ufcg.edu.br>
2022-10-19 17:46:03 -0300

    [ADD] novo componente sobre nos

---
+++ src/pages/AboutUs/index.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react'
+import styles from './styles.module.css'
+import CardGithub from '../../components/Card/CardGithub';
+import CardDiscord from '../../components/Card/CardDiscord';
+import Contribuitors from '../../components/Contributors'
+import { useTranslation } from 'react-i18next';
+
+export default function AboutUs() {
+
+    const { t } = useTranslation();
+    
+    return (
+        <>
+        <section className={styles.aboutUs}>
+            <section className={styles.firstStepAboutUS}>
+                <div className={styles.firstStepContentWrapper}>
+                    <h2>{t("about.mainTitle")}</h2>
+                    <p>{ t('about.firstPartDescription')}</p>
+                    <p>{ t('about.secondPartDescription')}</p>
+                </div>
+                <div className={styles.firstStepGraphWrapper}></div>
+            </section>
+
+            <section className={ styles.secondStepAboutUs}>
+                <div className={ styles.secondStepContentWrapper}>
+                    <h2>{t("about.collaborationsTitle")}</h2>
+                    <p>{t("about.collaborationsDescription")}</p>
+                </div>
+                <div className={ styles.contribuitorsWrapper}>
+                    <Contribuitors/>
+                </div>
+            </section>
+            <section className={styles.redirectCardsWrapper}>
+                <CardGithub/>
+                <CardDiscord/>
+            </section>
+
+        </section>
+        </>
+    )
+}
+
+
